79f3096a6d50c7afbd5a07b77684ee7e6e3ca2c2
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2018-10-26 21:17:49 +0530

    Fri 10/26/2018 21:17:36.46

---
+++ Ball.java
@@ -0,0 +1,12 @@
+public class Ball{
+    
+    public static global_time = 0; //global clock
+    private static num_of_balls = 0; //keep a record of num of balls
+
+    /*constructor*/ 
+    Ball(double x, double y, double speed, double angleOfSpeedWithX){
+        
+    }
+
+
+}
