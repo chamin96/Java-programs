3ccfc98997a6f63917d4e1ec8bd05d0c146adf7e
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2018-10-27 08:12:59 +0530

    commit on 27 Oct

--- ClassesAndObjects.java
+++ ClassesAndObjects.java
@@ -1,1 +1,20 @@
-e
+public class ClassesAndObjects{
+    public static void main(String[] args) {
+        Dog obj1 = new Dog();
+        obj1.bark();
+        obj1.walk();
+    }
+}
+
+class Dog{
+    String breed;
+    String color;
+
+    void bark(){
+        System.out.println("Barks");
+    }
+
+    void walk(){
+        System.out.println("Walks");
+    }
+}
