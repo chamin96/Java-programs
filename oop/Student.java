82dca307b578376205063ebe87bc7c5f63b02b49
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2018-11-20 11:11:52 +0530

    constructors

---
+++ oop/Student.java
@@ -0,0 +1,28 @@
+class Student{
+    static String name;
+    static int age;
+    static double gpa;
+
+    Student(){} //default constructor
+
+    //user defined constructor
+    Student(String n, int a, double g){
+        name = n;
+        age = a;
+        gpa = g;
+    }
+
+    void sleep(){
+
+    }
+
+    void eat(){
+
+    }
+
+    void info(){
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(gpa);
+    }
+}
